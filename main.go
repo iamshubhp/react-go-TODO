fea64769ce344e9ed6ad18582340faa7eb04b4be
iamshubhp <shubhpatel1812002@gmail.com>
2024-05-06 22:27:50 -0400

    Ready to Deploy

--- main.go
+++ main.go
@@ -15,47 +15,49 @@ import (
 )
 
 type Todo struct {
-	ID           primitive.ObjectID    `json:"id,omitempty" bson:"_id,omitempty"`
-	Completed    bool                  `json:"completed"`
-	Body         string 			   `json:"body"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body"`
 }
 
 var collection *mongo.Collection
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("hello world")
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+	if os.Getenv("ENV") != "production" {
+		// Load the .env file if not in production
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
 	}
 
-	// Get MongoDB URI from the environment variables
 	MONGODB_URI := os.Getenv("MONGODB_URI")
-
-	// Configure MongoDB client options
 	clientOptions := options.Client().ApplyURI(MONGODB_URI)
-
-	// Connect to the MongoDB database
 	client, err := mongo.Connect(context.Background(), clientOptions)
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer client.Disconnect(context.Background())
 
-	// Ping the database to check if the connection is successful
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Connected To Mongo DB")
+	fmt.Println("Connected to MONGODB ATLAS")
 
-	collection = client.Database("todo_db").Collection("todos")
+	collection = client.Database("golang_db").Collection("todos")
 
-	app := fiber.New()	
+	app := fiber.New()
+
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "http://localhost:5173",
+	// 	AllowHeaders: "Origin,Content-Type,Accept",
+	// }))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
@@ -67,7 +69,12 @@ func main() {
 		port = "5000"
 	}
 
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	}
+
 	log.Fatal(app.Listen("0.0.0.0:" + port))
+
 }
 
 func getTodos(c *fiber.Ctx) error {
@@ -94,6 +101,7 @@ func getTodos(c *fiber.Ctx) error {
 
 func createTodo(c *fiber.Ctx) error {
 	todo := new(Todo)
+	// {id:0,completed:false,body:""}
 
 	if err := c.BodyParser(todo); err != nil {
 		return err
@@ -124,7 +132,7 @@ func updateTodo(c *fiber.Ctx) error {
 	filter := bson.M{"_id": objectID}
 	update := bson.M{"$set": bson.M{"completed": true}}
 
-	_,err = collection.UpdateOne(context.Background(), filter, update)
+	_, err = collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		return err
 	}
